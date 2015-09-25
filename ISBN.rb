2aa3162ca762c48def10f19f2ff3818456e84800
casper22875 <casper22875@gmail.com>
2015-09-25 19:39:48 -0400

    added tests for string conversion and to check validity of 10 digits. added code for string to array 9 digits and check the checksum digit.

--- ISBN.rb
+++ ISBN.rb
@@ -1,6 +1,13 @@
 def isbn_check(isbn_number)
-	remove_dashes_from_isbn
-	verify_length
+	no_dashes_isbn_number =remove_dashes_from_isbn(isbn_number)
+	no_spaces_or_dashes_isbn_number=remove_dashes_from_isbn(no_dashes_isbn_number)
+		if verify_length(no_spaces_or_dashes_isbn_number) == true
+		array_isbn=isbn_number_array(isbn_number)
+		check_digit_validity(array_isbn)
+		true
+		else 
+		false
+	end
 end
 
 def verify_length(isbn_number)
@@ -24,3 +31,24 @@ def space_removal_from_isbn(isbn_number)
 		isbn_number.delete!" "
 	end
 end
+
+def isbn_number_array(isbn_number)
+	 isbn_number.split("")
+end
+
+def check_digit_validity(isbn_array_strings)
+	array=[]
+	isbn_array_strings.each do|value|
+	array<<value.to_i end
+	
+	sum=0
+		array.each.with_index do|value,index|
+		break if index == 9
+		sum += (value*(index+1))
+		end
+	check_digit = sum%11
+	check_digit == array[9]
+		
+			end
+		
+	
--- ISBN_test.rb
+++ ISBN_test.rb
@@ -17,8 +17,15 @@ def test_to_check_for_space_removal
 	assert_equal("0321146530",space_removal_from_isbn("0 321 14653 0"))
 end
 
+def test_for_string_conversion
+	assert_equal(["1","2","3","4","5","6","7","8","9","1"],isbn_number_array("1234567891"))
+end
 
-
+def test_for_10_digit_math
+	assert_equal(true,check_digit_validity(["0","4","7","1","9","5","8","6","9","7"]))
+	assert_equal(false,check_digit_validity(["1","3","4","5","6","3","2","1","8"]))
+	assert_equal(true,check_digit_validity(["0","3","2","1","1","4","6","5","3","0"]))
+end
 
 
 
